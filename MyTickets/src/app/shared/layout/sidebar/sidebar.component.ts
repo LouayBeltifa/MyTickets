0c0a6c3f80bac3b00b964529e980f104349dfac1
Louay BELTIFA <louay.beltifa@proxym-it.com>
2022-03-18 23:59:18 +0100

    Adding toggle button

--- MyTickets/src/app/shared/layout/sidebar/sidebar.component.ts
+++ MyTickets/src/app/shared/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,16 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
 
   links = [
-    {name:"Dashboard", route:"#Dashboard", class:"icon-home fs-18",active:"active"},
-    {name:"History", route:"#History", class:"icon-history fs-18"},
-    {name:"Validation", route:"#Validation", class:"icon-checkmark fs-18"},
-    {name:"Services", route:"#Services", class:"icon-dropbox fs-18"},
-    {name:"Logout", route:"#logout", class:"icon-exit fs-18"}
+    {name:"Dashboard", route:"#Dashboard", class:"icon-home fs-23",active:"active"},
+    {name:"History", route:"#History", class:"icon-history fs-23"},
+    {name:"Validation", route:"#Validation", class:"icon-checkmark fs-23"},
+    {name:"Services", route:"#Services", class:"icon-dropbox fs-23"},
+    {name:"Logout", route:"#logout", class:"icon-exit fs-23"}
   ]
+  public shouldShow = true;
+  @ViewChild("SidebarHeader") SidebarHeader: any;
+  @ViewChild("MainSidebar") MainSidebar: any;
+  @ViewChild("ClosedSideBar") ClosedSideBar: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggle(){
+    this.shouldShow = !this.shouldShow;
+    if (!this.shouldShow) {
+      this.SidebarHeader.nativeElement.classList.add("short");
+      this.MainSidebar.nativeElement.classList.add("hide");
+      this.ClosedSideBar.nativeElement.classList.add("sidebarClosed");
+      this.ClosedSideBar.nativeElement.classList.remove("hide");
+
+    }
+    else {
+      this.SidebarHeader.nativeElement.classList.remove("short");
+      this.MainSidebar.nativeElement.classList.remove("hide");
+      this.ClosedSideBar.nativeElement.classList.remove("sidebarClosed");
+      this.ClosedSideBar.nativeElement.classList.add("hide");
+
+    }
   }
 
 }
