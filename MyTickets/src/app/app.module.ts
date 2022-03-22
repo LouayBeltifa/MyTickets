fdb58a422a71c7f20d8e23d0197a21775b9f9b8b
Ameni Jaafer <jaafar.mahdoise@gmail>
2022-03-22 14:26:44 +0100

    add ticket widget

--- MyTickets/src/app/app.module.ts
+++ MyTickets/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TicketWidgetComponent } from './shared/components/genericComponents/ticket-widget/ticket-widget.component';
 import { WeatherWidgetComponent } from './shared/components/genericComponents/weather-widget/weather-widget.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,10 @@ import { WeatherWidgetComponent } from './shared/components/genericComponents/we
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CarouselModule,
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MyTickets/src/app/shared/components/genericComponents/ticket-widget/ticket-widget.component.ts
+++ MyTickets/src/app/shared/components/genericComponents/ticket-widget/ticket-widget.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+
 
 @Component({
   selector: 'app-ticket-widget',
   templateUrl: './ticket-widget.component.html',
-  styleUrls: ['./ticket-widget.component.scss']
+  styleUrls: ['./ticket-widget.component.scss'],
+ 
 })
 export class TicketWidgetComponent implements OnInit {
+  customOptions: OwlOptions = {
+    margin: 0,
+    navText:["",""],
+    center: true,
+    items:1,
+    loop:false,
+    responsive:{
+        600:{
+            items:4
+        }
+    },
+    nav: true
+  }
   url="../assets/Images/";
   ticketContent=[
-   {icon:`${this.url}bleuTicket.svg`,sold:"250",statut:"tickets en stock"},
-    {icon:`${this.url}yellowTicket.svg`,sold:"100",statut:"Ticket solde out"},
-    {icon:`${this.url}greenTicket.svg`,sold:"100 DT",statut:"Total earnings"}
-  ]
+   {id:1,icon:`${this.url}bleuTicket.svg`,sold:"250",statut:"tickets en stock"},
+    {id:2,icon:`${this.url}yellowTicket.svg`,sold:"100",statut:"Ticket solde out"},
+    {id:3,icon:`${this.url}greenTicket.svg`,sold:"100 DT",statut:"Total earnings"}
+  ];
+  
 
   constructor() { }
 
