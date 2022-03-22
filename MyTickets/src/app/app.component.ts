7c29ff0c8e62cedf0e4cbfdff7b9181bb3daf059
Louay BELTIFA <louay.beltifa@proxym-it.com>
2022-03-22 16:27:53 +0100

    fixing layout adding display content in the app component

--- MyTickets/src/app/app.component.ts
+++ MyTickets/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BreakpointState} from "@angular/cdk/layout";
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'MyTickets';
+export class AppComponent implements OnInit{
+
+  opened: boolean = false;
+  public showOverlay: boolean = false;
+  public ChangeDock: boolean | undefined;
+
+  constructor(public breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(['(max-width: 500px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.ChangeDock = true;
+        } else {
+          this.ChangeDock = false;
+        }
+      });
+  }
+
+  public toggleSidebar() {
+    this.opened = !this.opened;
+  }
+
+  public toggleSidebarMobile() {
+    this.opened = !this.opened;
+    this.showOverlay =!this.showOverlay;
+  }
+
+
 }
--- MyTickets/src/app/shared/layout/sidebar/sidebar.component.ts
+++ MyTickets/src/app/shared/layout/sidebar/sidebar.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidebar',
@@ -15,34 +14,14 @@ export class SidebarComponent implements OnInit {
     {name:"Services", route:"#Services", class:"icon-dropbox fs-23"},
     {name:"Logout", route:"#logout", class:"icon-exit fs-23"}
   ]
-
-  opened: boolean = false;
-  public ChangeDock: boolean | undefined;
-  public showOverlay: boolean = false;
-
-
-  constructor(public breakpointObserver: BreakpointObserver) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.breakpointObserver
-      .observe(['(max-width: 500px)'])
-      .subscribe((state: BreakpointState) => {
-        if (state.matches) {
-          this.ChangeDock = true;
-        } else {
-          this.ChangeDock = false;
-        }
-      });
+
   }
 
 
 
-  public toggleSidebar() {
-      this.opened = !this.opened;
- }
 
- public toggleSidebarMobile(){
-   this.opened = !this.opened;
-   this.showOverlay =!this.showOverlay;
- }
+
 }
