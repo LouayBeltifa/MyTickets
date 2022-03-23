543a5ae1f527ead718417610a546f6c38b00e7d4
Louay BELTIFA <louay.beltifa@proxym-it.com>
2022-03-23 11:47:18 +0100

    fixing some bugs

--- MyTickets/src/app/app.module.ts
+++ MyTickets/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TicketWidgetComponent } from './shared/components/genericComponents/ticket-widget/ticket-widget.component';
 import { WeatherWidgetComponent } from './shared/components/genericComponents/weather-widget/weather-widget.component';
-import { CarouselModule } from 'ngx-owl-carousel-o';
 import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
 import { UserHeaderComponent } from './shared/layout/user-header/user-header.component';
+import {CarouselModule} from "ngx-owl-carousel-o";
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import {NotificationComponent} from "./shared/layout/user-header/notification/notification.component";
